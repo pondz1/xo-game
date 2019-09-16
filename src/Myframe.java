68ad3b1b62c831684c7cdf4cdbe7befee52cf953
pondxyz <ponejirasuk@gmail.com>
2019-09-17 00:54:04 +0700

    don't remove

--- src/Myframe.java
+++ src/Myframe.java
@@ -4,27 +4,48 @@ import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.Random;
+import java.util.ArrayList;
+
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
 public class Myframe extends JFrame{
 	
+	JPanel pa = new JPanel();
 	Button bt[] = new Button[9];
 	int count = 0;
-	Random ran = new Random();
-	int num[] = {0,1,2,3,4,5,6,7,8};
+	ArrayList<Integer> arrX = new ArrayList<Integer>();
+	ArrayList<Integer> arrO = new ArrayList<Integer>();
+	
+	ArrayList<Integer> lineTop = new ArrayList<Integer>();
+	ArrayList<Integer> lineMid = new ArrayList<Integer>();
+	ArrayList<Integer> lineBot = new ArrayList<Integer>();
+	
+	ArrayList<Integer> lineleft = new ArrayList<Integer>();
+	ArrayList<Integer> lineMidV = new ArrayList<Integer>();
+	ArrayList<Integer> lineright = new ArrayList<Integer>();
+	
+	ArrayList<Integer> left = new ArrayList<Integer>();
+	ArrayList<Integer> right = new ArrayList<Integer>();
+	
+	int input = 100;
 	
 	public Myframe(String str) {
 		setTitle(str);
-		setLayout(new GridLayout(3, 3));
+		setLayout(new GridLayout(0, 1));
 		setBounds(300, 200, 400, 400);
+		pa.setLayout(new GridLayout(3, 3));
+		//pa.setBounds(0, 0, 400, 400);
+		add(pa);
+		theCondition();
 		createButton();
 		
 	}
 	void createButton() {
 		for (int i = 0; i < bt.length; i++) {
 			bt[i] = new Button();
-			this.add(bt[i]);
+			pa.add(bt[i]);
 			onClick(i);
 		}
 	}
@@ -34,12 +55,14 @@ public class Myframe extends JFrame{
 		bt[i].setFont(new Font("Tahoma", Font.BOLD, 100));
 		bt[i].setForeground(Color.BLACK);
 		bt[i].setEnabled(false);
+		arrX.add(i);
 	}
 	void setO(int i) {
 		bt[i].setLabel("O");
 		bt[i].setBackground(Color.RED);
 		bt[i].setFont(new Font("Tahoma", Font.BOLD, 100));
 		bt[i].setEnabled(false);
+		arrO.add(i);
 	}
 	void onClick(int i) {
 		bt[i].addActionListener(new ActionListener() {
@@ -49,12 +72,47 @@ public class Myframe extends JFrame{
 				if (count%2 == 0) {
 					setTitle("Next Player O");
 					setX(i);
+					//System.out.println(arrX);
 				}else {
 					setTitle("Next Player X");
 					setO(i);
 				}
 				count++;
+				checkWin();
 			}
 		});
+	}
+	void theCondition() {
+		lineTop.add(0);lineTop.add(1);lineTop.add(2);
+		lineMid.add(3);lineMid.add(4);lineMid.add(5);
+		lineBot.add(6);lineBot.add(7);lineBot.add(8);
+		
+		lineleft.add(0);lineleft.add(3);lineleft.add(6);
+		lineMidV.add(1);lineMidV.add(4);lineMidV.add(7);
+		lineright.add(2);lineright.add(5);lineright.add(8);
+		
+		left.add(0);left.add(4);left.add(8);
+		right.add(2);right.add(4);right.add(6);
+	}
+	void checkWin() {
+		
+		String  message = " "; 
+		boolean xWin = (arrX.containsAll(lineTop) || arrX.containsAll(lineMid) || arrX.containsAll(lineBot) || arrX.containsAll(lineleft) || arrX.containsAll(lineMidV) || arrX.containsAll(lineright) || arrX.containsAll(left) || arrX.containsAll(right));
+		boolean oWin = (arrO.containsAll(lineTop) || arrO.containsAll(lineMid) || arrO.containsAll(lineBot) || arrO.containsAll(lineleft) || arrO.containsAll(lineMidV) || arrO.containsAll(lineright) || arrO.containsAll(left) || arrO.containsAll(right)); 
+		if (xWin) {
+			message = "X win" ;
+		    input = JOptionPane.showConfirmDialog(null, message, "Message", JOptionPane.DEFAULT_OPTION);
+		    reset(input);
+		}else if (oWin) {
+			message = "O win" ;
+			input = JOptionPane.showConfirmDialog(null, message, "Message", JOptionPane.DEFAULT_OPTION);
+			reset(input);
+		}
+
+	}
+	void reset(int d) {
+		if (JOptionPane.OK_CANCEL_OPTION == d) {
+			pa.removeAll();
+		}
 	}
 }
